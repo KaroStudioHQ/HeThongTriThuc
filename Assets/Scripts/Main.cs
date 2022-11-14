8083d631a746846882492c4b62f1866610fdb1d9
Karo <lequang2000hn@gmail.com>
2022-11-14 16:51:53 +0700

    code

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+
+    class NeuralNetwork
+    {
+        int node_input;
+        int node_hidden;
+        int node_output;
+        string activation = "hyperbol";
+
+        public NeuralNetwork(int node_input, int node_hidden, int node_output, string activation = "hyperbol")
+        {
+
+        }
+    }
+}
